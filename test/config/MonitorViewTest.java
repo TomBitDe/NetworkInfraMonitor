7e434689a430e39ceeab06714874f672d39a5f8a
TomBitDe <tbit455674@gmail.com>
2017-05-22 18:14:38 +0200

    Comment improved.

--- test/config/MonitorViewTest.java
+++ test/config/MonitorViewTest.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package config;
 
 import java.util.ArrayList;
@@ -15,8 +10,7 @@ import org.junit.BeforeClass;
 import org.junit.Test;
 
 /**
- *
- * @author Tom
+ * Test the class MonitorView.
  */
 public class MonitorViewTest {
 
